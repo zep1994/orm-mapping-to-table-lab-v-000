7583a194014794c5c2fc711299943dfefd12c28a
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 05:15:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        grade TEXT
+        grade INTEGER
         )
         SQL
     DB[:conn].execute(sql)
