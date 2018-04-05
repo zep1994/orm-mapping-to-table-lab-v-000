8417d3717e402cefef388057e2f460cf73414e67
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 05:09:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,8 +28,8 @@ class Student
 
   def save
   sql = <<- SQL
-    INSERT INTO students (name, grade)
-    VALUES (?, ?)
+   INSERT INTO songs (name, grade) 
+   VALUES (?, ?)
   SQL
 
   DB[:conn].execute(sql, self.name, self.grade)
