29225f05b5c924aa6ce65e398304029742ed6fcf
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 02:59:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,9 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  attr_accessor :name, :grade
+ 
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+ 
 end
