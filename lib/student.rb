3730f046d7fc6c9fa339e817f0d6d15f3502dab1
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 05:14:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(id=nil, name, grade)
+  def initialize(id= nil, name, grade)
     @id = id
     @name = name
     @grade = grade
