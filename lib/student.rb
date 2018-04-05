8a928fea0e1732824f5003df4f0f878942affa9b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 04:46:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,9 +13,21 @@ class Student
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        album TEXT
+        grade TEXT
         )
         SQL
     DB[:conn].execute(sql)
   end
+
+  def self.drop_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+  
 end
