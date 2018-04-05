a180d8201454047a3144a950cf098d8c371ff602
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 05:01:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,11 +21,7 @@ class Student
 
   def self.drop_table
     sql =  <<-SQL
-      CREATE TABLE IF EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
+      DROP TABLE IF EXISTS students 
         SQL
     DB[:conn].execute(sql)
   end
