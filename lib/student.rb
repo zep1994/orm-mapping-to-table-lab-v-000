17a9abb3400ee1562c7c3d950b3e1c92e6284a14
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-05 04:38:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,4 +8,14 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
 end
